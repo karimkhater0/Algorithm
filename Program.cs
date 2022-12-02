4fad507ec5549ba268028d7066038f00c1bffaf2
karimkhater0 <95217764+karimkhater0@users.noreply.github.com>
2022-12-02 21:25:49 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Insertion_Sort
+{
+    class insertion_Sort
+    {
+        static void Main(string[] args)
+        {
+
+           
+
+            List<int> l = new List<int>{ 5, 22, 8, 4, 1, 25 };
+            Console.WriteLine("Before sorting:");
+            l.ForEach(Console.WriteLine);
+            Console.WriteLine("After sorting:");
+            insertionSort(l);
+            l.ForEach(Console.WriteLine);
+            Console.ReadLine();
+        }
+
+        static List<int>  insertionSort(List<int> list)
+        {
+            for (int j = 1; j < list.Count; j++)
+            {
+                int key = list[j];
+
+                int i = j - 1;
+
+                while (i >= 0 && list[i] > key)
+                {
+                    list[i + 1] = list[i];
+                    i = i - 1;
+                    list[i + 1] = key;
+                }
+            }
+            return list;
+        }
+        }
+}
